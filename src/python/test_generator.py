2569aa1829a7133f3692b642d1b85b9630ce13da
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-02 16:26:32 -0700

    many errors fixed, first test ready

--- src/python/test_generator.py
+++ src/python/test_generator.py
@@ -1,14 +1,15 @@
 """
-These are tests for generator methods. There are three methods to be tested:
+These are tests for generator methods. There are five methods to be tested:
     make_inliers
     add_gaussian_noise
     add_laplace_noise
     add_structural_bias
-
+    add_outliers
+    
 Run from terminal as follows:
     $ python -m unittest test_generator.py
 """
-import generator.py
+import generator
 
 import unittest
 
@@ -16,15 +17,7 @@ class TestMakeInliers(unittest.TestCase):
     """
     For make_inliers, I am testing the following cases. Assume, n_points = n + 1.
 
-    Happy path:
-        slope =  1, intercept = 0, x_min = 0, x_max = n, y_min = 0, y_max = n
-            should create two identical list of n points [0, .. n],
-            corresponding to points (0, 0), ... , (n, n)
-        Tests:
-            should creates n_points points
-            all x should be within x_min and x_max
-            all y should be within y_min and y_max
-            
+    Happy paths:
         slope = -1, intercept = n should create two identical list of [n, ..., 0]
             should create two identical list of n points [0, .. n],
             corresponding to points (0, 0), ... , (n, n)
@@ -50,16 +43,41 @@ class TestMakeInliers(unittest.TestCase):
         n = 0 should return an empty list
         n = 1 should return an empty list
         if x_min = x_max,
-            if y_min = y_max, return empty list
-            else, return a list of points with same x.
-        if y_min = y_max,
-            if x_min = x_max, return empty list
-            else, return a list of points with same y.
+            return empty list
     """
-    # Edge case 0 slope
+    
     def test_unit_slope_zero_intercept(self):
-        result = make_inliers(...)
-        self.assertEqual(result, expected)
+        """
+        slope =  1, intercept = 0, x_min = 0, x_max = n, y_min = 0, y_max = n
+            should create two identical list of n points [0, .. n],
+            corresponding to points (0, 0), ... , (n, n)
+        Tests:
+            should creates n_points points
+            all x should be within x_min and x_max
+            all y should be within y_min and y_max
+        """
+        # initiate all arguments
+        n_inliers = 10
+        data_x = [float("INF")] * n_inliers
+        data_y = [float("INF")] * n_inliers
+        slope = 1
+        intercept = 0
+        x_min = 0
+        x_max = n_inliers - 1
+
+        # fill data_x, data_y, passing them to make_inliers
+        generator.make_inliers(data_x, data_y, n_inliers, slope, intercept,
+                              x_min, x_max)
+
+        # expected values
+        exp_x = [float(i) for i in range(n_inliers)]
+        exp_y = [float(i) for i in range(n_inliers)]      
+
+        # assertions
+        for i in range(n_inliers):
+            self.assertAlmostEqual(data_x[i], exp_x[i])
+            self.assertAlmostEqual(data_y[i], exp_y[i])
+
 
 if __name__ == '__main__':
     unittest.main()
