55dc8429fc36c26b1612cb4537819d4faa4332d2
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-06 05:05:37 -0700

    add stitch_graph and helper find_inliers

--- src/python/model.py
+++ src/python/model.py
@@ -1,9 +1,12 @@
 """
 Model fitting functions for RANSAC line fitting pipeline. Provides a stitching
 function for combining inliers from two overlapping graphs into a single
-refined model analogous to what will be done in homograhy.Also provides two
-suport functions for use in the RANSAC algorithm: least squares line fitting
-and perpendicular distance computation.
+refined model analogous to what will be done in homograhy.
+Also provides two suport functions for use in the RANSAC algorithm:
+    least squares line fitting and
+    perpendicular distance computation.
+Also provides a function to estimate the euclidean distance of estimated model
+from the known true model, if known.
 
 Functions:
     stitch_models               combines inliers from two graphs into a
@@ -11,9 +14,47 @@ Functions:
     fit_line                    fits a line to n points using least squares
     points_to_line_distances    computes perpendicular distances from points
                                 to a line
+    model_error                 measures Euclidean distance between estimated
+                                and true model parameters
+
 """
 
 import math
+
+def find_inliers(points_x, points_y, n_points, slope, intercept, threshold,
+                 inliers_x, inliers_y):
+    """
+    Suport function for stitch_graphs. Collects inliers from points_x and
+    points_y by computing perpendicular distance from each point to the line
+    defined by slope and intercept. Points within threshold distance are
+    appended to inlier_x and inlier_y in place.
+
+    Params:
+        points_x    a list of n_points floats
+        points_y    a list of n_points floats
+        n_points    int, number of points to evaluate
+        slope       float, slope of the model line
+        intercept   float, intercept of the model line
+        threshold   float, maximum perpendicular distance to qualify as inlier
+        inlier_x    a list of floats, appended to in place
+        inlier_y    a list of floats, appended to in place
+
+    Returns:
+        0 for success
+        -1 for error if n_points < 1
+        -1 for error if threshold <= 0
+    """
+    if n_points < 1 or threshold <= 0:
+        return -1
+    distances = [float("inf")] * n_points
+    points_to_line_distances(points_x, points_y, n_points,
+                                   slope, intercept, distances)
+    for i in range(n_points):
+        if distances[i] < threshold:
+            inliers_x.append(points_x[i])
+            inliers_y.append(points_y[i])
+    return 0
+
 
 def stitch_models(points_x1, points_y1, n1,
                   slope1, intercept1,
@@ -54,7 +95,39 @@ def stitch_models(points_x1, points_y1, n1,
         -1 for error if pos < 0
         -1 for error if no inliers found in either graph
     """
-    
+    if n1 < 2 or n2 < 2 or threshold <= 0 or pos < 0:
+        return -1
+    # find inliers from graph1    
+    inliers_x1 = []
+    inliers_y1 = []
+    find_inliers(points_x1, points_y1, n1, slope1, intercept1, threshold,
+                 inliers_x1, inliers_y1)
+    n_inliers1 = len(inliers_x1)
+    if n_inliers1 == 0:
+        return -1
+    # find inliers from graph2
+    inliers_x2 = []
+    inliers_y2 = []
+    find_inliers(points_x2, points_y2, n2, slope2, intercept2, threshold,
+                 inliers_x2, inliers_y2)
+    n_inliers2 = len(inliers_x2)
+    if n_inliers2 == 0:
+        return -1
+    # find all inliers
+    n_inliers =  n_inliers1 + n_inliers2
+    inliers_x = [float("inf")] * (n_inliers)
+    inliers_y = [float("inf")] * (n_inliers)
+    for i in range(n_inliers):
+        if i < n_inliers1:
+            inliers_x[i] = inliers_x1[i]
+            inliers_y[i] = inliers_y1[i]
+        else:
+            inliers_x[i] = inliers_x2[i - n_inliers1]
+            inliers_y[i] = inliers_y2[i - n_inliers1]
+    # fit_line on all inliers
+    fit_line(inliers_x, inliers_y, n_inliers, list_slopes, list_intercepts, pos)
+    return 0
+
 
 def fit_line(points_x, points_y, n_points, list_slopes, list_intercepts, pos):
     """
