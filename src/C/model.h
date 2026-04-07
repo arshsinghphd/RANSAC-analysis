d6cf0d4ce0fe7c8237ac6dd561a37694d0a68c83
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-07 16:25:56 -0700

    create model.h; add fit_model.

---
+++ src/C/model.h
@@ -0,0 +1,63 @@
+/**
+ * Model fitting functions for RANSAC pipeline. Supports polynomial models of
+ * any degree via least squares solved using Gaussian elimination on the normal
+ * equations. Provides stitching of two overlapping graphs into a single refined
+ * model, analogous to image stitching in homography estimation.
+ * 
+ * Model parameters are stored as polynomial coefficients from lowest to highest
+ * degree:
+ * params[pos * n_params + 0] = a0  (intercept / constant term)
+ * params[pos * n_params + 1] = a1  (slope / linear term)
+ * params[pos * n_params + 2] = a2  (quadratic term)
+ * ...
+ * For line fitting n_params = 2, giving y = a0 + a1 * x.
+ * For quadratic fitting n_params = 3, giving y = a0 + a1 * x + a2 * x^2.
+ * 
+ * Functions:
+ * eval_model                  evaluates polynomial model at a given x
+ * fit_model                   fits polynomial model using least squares
+ * find_model_inliers          collects inliers using vertical residual
+ * stitch_models               combines inliers from two graphs into one model
+ * points_to_line_distances    computes perpendicular distances to a line
+ * model_error                 measures Euclidean distance between two models
+ * 
+ * References:
+ * Fischler, M. A. and Bolles, R. C. 1981. Random sample consensus: a paradigm
+ * for model fitting with applications to image analysis and automated
+ * cartography. Commun. ACM 24, 6, 381-395.
+ */
+
+/**
+ * Fits a polynomial model of degree n_params - 1 to n_points data points using
+ * least squares, solved via Gaussian elimination on the normal equations. 
+ * Stores the n_params coefficients in params starting at index pos * n_params, 
+ * in order from lowest to highest degree:
+ * params[pos * n_params + 0] = a0  (intercept) 
+ * params[pos * n_params + 1] = a1  (slope for line)
+ * params[pos * n_params + 2] = a2  (quadratic term)
+ * ...
+ * For n_params = 2 this is equivalent to ordinary least squares line fitting:
+ * y = a0 + a1 * x
+ * The normal equations (X^T X) a = X^T y are formed using the Vandermonde
+ * matrix X and solved using Gaussian elimination with partial pivoting and
+ * back substitution. All matrix operations are implemented without external
+ * libraries for C-correspondence.
+ * 
+ * Params:
+ * points_x     a list of n_points floats
+ * points_y     a list of n_points floats
+ * n_points     int, number of points to fit
+ * params       a list of floats of size at least (pos + 1) * n_params, 
+ *              modified in place
+ * n_params     int, number of model parameters, equals polynomial degree + 1
+ * 
+ * Returns:
+ * 0 for success
+ * -1 for error if n_points < n_params
+ * -1 for error if n_params < 2-1 for error if pos < 0
+ * -1 for error if matrix is singular (e.g. all x values equal)
+ */
+int fit_model(float* points_x, float* points_y, int n_points, float* params, 
+    int n_params);
+
+
