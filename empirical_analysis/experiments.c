1cdaa38b1368ea63a9e4514964f586a6f7c02319
arshsinghphd <57355255+arshsinghphd@users.noreply.github.com>
2026-04-14 08:52:55 -0700

    fix spacing

--- empirical_analysis/experiments.c
+++ empirical_analysis/experiments.c
@@ -21,7 +21,7 @@ float bias_linear_bias(float x) {
 }
 
 float bias_periodic(float x) {
-    return sinf(2.0f * M_PI * x);  // since we are scaling data [0 .. 1]
+    return sinf(2.0f * M_PI * x); // since we are scaling data [0 .. 1]
 }
 
 
@@ -29,21 +29,20 @@ float bias_periodic(float x) {
  * make_data
  * --------------------------------------------------------------- */
 void make_data(float* points_x, float* points_y,
-               int n_inliers, int n_outliers,
-               const float* true_params, int n_params,
-               float noise_std,
-               float* t_out,
-               int apply_bias,
-               float (*bias_fn)(float), float pr) {
+        int n_inliers, int n_outliers,
+        const float* true_params, int n_params,
+        float noise_std,
+        float* t_out,
+        int apply_bias,
+        float (*bias_fn)(float), float pr) {
 
     /* copy true_params into a mutable local array */
     float params[n_params];
     for (int j = 0; j < n_params; j++)
-        params[j] = true_params[j];
+    params[j] = true_params[j];
 
     /* generate inliers on the true polynomial model */
-    make_inliers(points_x, points_y, n_inliers,
-                 params, n_params, X_MIN, X_MAX);
+    make_inliers(points_x, points_y, n_inliers, params, n_params, X_MIN, X_MAX);
 
     /* add gaussian noise if noise_std > 0, else clean data */
     if (noise_std > 0.0f)
@@ -56,17 +55,21 @@ void make_data(float* points_x, float* points_y,
     /* estimate threshold from inlier data before outliers are added */
     float t = compute_t(points_x, points_y, n_inliers, n_params);
     if (t < 1e-6f)
-        t = 2.0f * noise_std
-            * sqrtf(1.0f + true_params[1] * true_params[1]);
+        t = 2.0f * noise_std * sqrtf(1.0f + true_params[1] * true_params[1]);
     if (t < 1e-6f)
         t = 1e-4f;
     *t_out = t;
 
     /* append outliers after threshold is estimated */
-    if (n_outliers > 0)
-        add_outliers(points_x, points_y, n_inliers, n_outliers,
-                     params, n_params, noise_std > 0.0f ? noise_std
-                                                        : NOISE_STD);
+    if (n_outliers > 0){
+        if (noise_std > 0) {
+            add_outliers(points_x, points_y, n_inliers, n_outliers, 
+                params, n_params, noise_std);
+        } else {
+            add_outliers(points_x, points_y, n_inliers, n_outliers, 
+                params, n_params, NOISE_STD);
+        }
+    }
 }
 
 
@@ -74,84 +77,84 @@ void make_data(float* points_x, float* points_y,
  * run_ransac
  * --------------------------------------------------------------- */
 RansacResult run_ransac(float* points_x, float* points_y,
-                         int n_points, int n_params,
-                         const float* true_params,
-                         float epsilon, float t, int d, int k,
-                         int repeat, int index) {
-    RansacResult res;
-    res.index       = index;
-    res.n           = n_points;
-    res.m           = n_params;
-    res.epsilon     = epsilon;
-    res.t           = t;
-    res.d           = d;
-    res.k           = k;
-    res.repeat      = repeat;
-    res.model_error = -1.0f;
-    res.time_mu_s     = 0.0f;
+       int n_points, int n_params,
+       const float* true_params,
+       float epsilon, float t, int d, int k,
+       int repeat, int index) {
+ RansacResult res;
+ res.index  = index;
+ res.n   = n_points;
+ res.m   = n_params;
+ res.epsilon  = epsilon;
+ res.t   = t;
+ res.d   = d;
+ res.k   = k;
+ res.repeat  = repeat;
+ res.model_error = -1.0f;
+ res.time_mu_s  = 0.0f;
 
-    float return_array[2 + n_params];
+ float return_array[2 + n_params];
 
-    /* time the ransac call */
-    clock_t start = clock();
-    int ret = ransac(points_x, points_y, n_points, n_params,
-                     k, t, d, return_array);
-    clock_t end = clock();
+ /* time the ransac call */
+ clock_t start = clock();
+ int ret = ransac(points_x, points_y, n_points, n_params,
+      k, t, d, return_array);
+ clock_t end = clock();
 
-    res.time_mu_s = (float)(end - start) / CLOCKS_PER_SEC * 1e6f;
+ res.time_mu_s = (float)(end - start) / CLOCKS_PER_SEC * 1e6f;
 
-    if (ret == -1)
-        return res;
+ if (ret == -1)
+  return res;
 
-    /* compute model error */
-    float est_params[n_params];
-    float true_p[n_params];
-    for (int j = 0; j < n_params; j++) {
-        est_params[j] = return_array[2 + j];
-        true_p[j]     = true_params[j];
-    }
-    res.model_error = model_error(est_params, true_p, n_params);
+ /* compute model error */
+ float est_params[n_params];
+ float true_p[n_params];
+ for (int j = 0; j < n_params; j++) {
+  est_params[j] = return_array[2 + j];
+  true_p[j]  = true_params[j];
+ }
+ res.model_error = model_error(est_params, true_p, n_params);
 
-    return res;
+ return res;
 }
 
 
 /* Development tests */
 
 // int main(void) {
-//     srand((unsigned int) time(NULL));
+//  srand((unsigned int) time(NULL));
 
-//     printf("experiments.c sanity check\n");
-//     printf("---------------------------\n");
+//  printf("experiments.c sanity check\n");
+//  printf("---------------------------\n");
 
-//     /* linear model, epsilon = 0.3 */
-//     float true_params[] = _TRUE_PARAMS_LINEAR;
-//     int   n_params  = 2;
-//     float epsilon   = 0.3f;
-//     int   n_inliers = (int)((1.0f - epsilon) * N_TOTAL);
-//     int   n_outliers = N_TOTAL - n_inliers;
-//     int   d         = compute_d(epsilon, N_TOTAL);
-//     int   k         = compute_k(epsilon, n_params, FAIL_PROB);
+//  /* linear model, epsilon = 0.3 */
+//  float true_params[] = _TRUE_PARAMS_LINEAR;
+//  int n_params = 2;
+//  float epsilon = 0.3f;
+//  int n_inliers = (int)((1.0f - epsilon) * N_TOTAL);
+//  int n_outliers = N_TOTAL - n_inliers;
+//  int d   = compute_d(epsilon, N_TOTAL);
+//  int k   = compute_k(epsilon, n_params, FAIL_PROB);
 
-//     float points_x[N_TOTAL], points_y[N_TOTAL];
-//     float t;
+//  float points_x[N_TOTAL], points_y[N_TOTAL];
+//  float t;
 
-//     make_data(points_x, points_y, n_inliers, n_outliers,
-//               true_params, n_params, NOISE_STD, &t,
-//               0, NULL, 0.0f);
+//  make_data(points_x, points_y, n_inliers, n_outliers,
+//    true_params, n_params, NOISE_STD, &t,
+//    0, NULL, 0.0f);
 
-//     printf("n_inliers  = %d\n", n_inliers);
-//     printf("n_outliers = %d\n", n_outliers);
-//     printf("t          = %.4f\n", t);
-//     printf("d          = %d\n", d);
-//     printf("k          = %d\n", k);
+//  printf("n_inliers = %d\n", n_inliers);
+//  printf("n_outliers = %d\n", n_outliers);
+//  printf("t   = %.4f\n", t);
+//  printf("d   = %d\n", d);
+//  printf("k   = %d\n", k);
 
-//     RansacResult res = run_ransac(points_x, points_y, N_TOTAL,
-//                                   n_params, true_params,
-//                                   epsilon, t, d, k, 0, 0);
+//  RansacResult res = run_ransac(points_x, points_y, N_TOTAL,
+//         n_params, true_params,
+//         epsilon, t, d, k, 0, 0);
 
-//     printf("model_error = %.6f\n", res.model_error);
-//     printf("time_mu_s     = %.2f\n", res.time_mu_s);
+//  printf("model_error = %.6f\n", res.model_error);
+//  printf("time_mu_s  = %.2f\n", res.time_mu_s);
 
-//     return 0;
+//  return 0;
 // }
